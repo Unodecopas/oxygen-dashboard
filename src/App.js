31cd82773989588e505ce512d1b95c4037aa32f3
Jesus Gallardo <gallardomoralesjesus@gmail.com>
2022-09-20 17:48:59 +0200

    fix: routes

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ import ProtectedRoute from './components/ProtectedRoute'
 import LoginPage from './pages/LoginPage'
 import HomePage from './pages/HomePage'
 import styled from 'styled-components'
-import DashboardPage from './pages/DashboardPage'
-import RoomsPage from './pages/RoomsPage'
-import BookingsPage from './pages/BookingsPage'
-import ConciergePage from './pages/ConciergePage'
-import GuestListPage from './pages/GuestPage'
 
 const Dashboard = styled.div`
   width:100%;
@@ -22,13 +17,7 @@ function App () {
       <Routes>
         <Route path='login' element={<LoginPage setUser={setUser} />} />
         <Route element={<ProtectedRoute user={user}/>}>
-          <Route path='/' element={<HomePage />}>
-            <Route path='dashboard' element={<DashboardPage />} />
-            <Route path='guest' element={<GuestListPage />} />
-            <Route path='rooms' element={<RoomsPage />} />
-            <Route path='bookings' element={<BookingsPage />} />
-            <Route path='concierge' element={<ConciergePage />} />
-          </Route>
+          <Route path='/*' element={<HomePage />} />
         </Route>
       </Routes>
     </Dashboard>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import styled from 'styled-components'
 import Header from '../components/Header'
 import Navbar from '../components/Navbar'
-
+import BookingsPage from './BookingsPage'
+import ConciergePage from './ConciergePage'
+import DashboardPage from './DashboardPage'
+import GuestPage from './GuestPage'
+import RoomsPage from './RoomsPage'
 const HomePageContainer = styled.div`
   display: grid;
   grid-template-columns: 354px 1fr;
@@ -12,14 +17,20 @@ const MainContainer = styled.div`
   display:flex;
   flex-direction: column;
 `
-const HomePage = ({ children }) => {
+const HomePage = () => {
   return (
     <HomePageContainer>
       <Navbar />
       <MainContainer>
         <Header />
         <main>
-          {children}
+          <Routes>
+            <Route path='/dashboard' element={<DashboardPage />} />
+            <Route path='/guest' element={<GuestPage />} />
+            <Route path='/rooms' element={<RoomsPage />} />
+            <Route path='/bookings' element={<BookingsPage />} />
+            <Route path='/concierge' element={<ConciergePage />} />
+          </Routes>
         </main>
       </MainContainer>
     </HomePageContainer>
