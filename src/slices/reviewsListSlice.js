db4290f61c1e37fc6054a64a6354b4c4b5503fc6
Jesus Gallardo <gallardomoralesjesus@gmail.com>
2022-10-05 19:18:16 +0200

    feat: add update, delete and new Slices for reviews

--- src/slices/reviewsListSlice.js
+++ src/slices/reviewsListSlice.js
@@ -7,8 +7,32 @@ export const fetchReviews = createAsyncThunk('reviews/fetchReviews', async () =>
   return delayedReviews
 })
 
-export const fetchBooking = createAsyncThunk(
+export const fetchReview = createAsyncThunk(
   'reviews/fetchReview',
+  async (id) => {
+    const findReviews = reviewsData.find((element) => element.id === Number(id))
+    const review = await delay(findReviews)
+    return review
+  }
+)
+export const updateReview = createAsyncThunk(
+  'reviews/updateReview',
+  async (id) => {
+    const findReviews = reviewsData.find((element) => element.id === Number(id))
+    const review = await delay(findReviews)
+    return review
+  }
+)
+export const deleteReview = createAsyncThunk(
+  'reviews/deleteReview',
+  async (id) => {
+    const findReviews = reviewsData.find((element) => element.id === Number(id))
+    const review = await delay(findReviews)
+    return review
+  }
+)
+export const newReview = createAsyncThunk(
+  'reviews/newReview',
   async (id) => {
     const findReviews = reviewsData.find((element) => element.id === Number(id))
     const review = await delay(findReviews)
@@ -33,6 +57,47 @@ export const reviewsListSlice = createSlice({
         state.reviews = action.payload
       })
       .addCase(fetchReviews.rejected, (state) => {
+        state.status = 'error'
+      })
+      .addCase(fetchReview.pending, (state) => {
+        state.status = 'loading'
+      })
+      .addCase(fetchReview.fulfilled, (state, action) => {
+        state.status = 'fulfilled'
+        state.reviews = action.payload
+      })
+      .addCase(fetchReview.rejected, (state) => {
+        state.status = 'error'
+      })
+      .addCase(updateReview.pending, (state) => {
+        state.status = 'loading'
+      })
+      .addCase(updateReview.fulfilled, (state, action) => {
+        state.status = 'fulfilled'
+        state.reviews = state.review.filter((review) => review.id !== action.payload.id)
+        state.reviews.push(action.payload)
+      })
+      .addCase(updateReview.rejected, (state) => {
+        state.status = 'error'
+      })
+      .addCase(deleteReview.pending, (state) => {
+        state.status = 'loading'
+      })
+      .addCase(deleteReview.fulfilled, (state, action) => {
+        state.status = 'fulfilled'
+        state.reviews = state.reviews.filter((review) => review.id !== action.payload.id)
+      })
+      .addCase(deleteReview.rejected, (state) => {
+        state.status = 'error'
+      })
+      .addCase(newReview.pending, (state) => {
+        state.status = 'loading'
+      })
+      .addCase(newReview.fulfilled, (state, action) => {
+        state.status = 'fulfilled'
+        state.reviews.push(action.payload)
+      })
+      .addCase(newReview.rejected, (state) => {
         state.status = 'error'
       })
   }
